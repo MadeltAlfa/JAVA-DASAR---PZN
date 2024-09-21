e427cd8ed45a67e7bd0069b67603ce0143f96ec9
Nanda Bagus Ramadhani <nannraamdh@gmail.com>
2024-09-21 21:52:16 +0700

    first commit

---
+++ src/KonversiNumber.java
@@ -0,0 +1,15 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+
+        // kecil ke besar
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+        long iniLong = iniInt;
+        float iniFloat = iniLong;
+        double iniDouble = iniFloat;
+
+        // besar ke kecil
+        float iniFloat2 = (float) iniDouble;
+    }
+}
